6b6ccd52c09e24d71a6f9e17048567d1312280f4
reeman9000 <preston.soper@student.awesomeinc.org>
2023-03-27 21:48:59 +0000

    made admin portal and created tables

--- sandbox/sandbox/settings.py
+++ sandbox/sandbox/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io']
